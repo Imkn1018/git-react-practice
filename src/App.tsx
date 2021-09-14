7363feeda590a66744ecb343668fca680e4cbe9e
今井健太郎 <imaikentaro@bitA01287.local>
2021-09-14 14:56:07 +0900

    feat:　todoリストの表示

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './App.css';
 
 interface Todo {
   value: string;
+  id: number;
 }
 
 function App() {
@@ -16,16 +17,24 @@ function App() {
 
     const newTodo: Todo = {
       value: text,
+      id: new Date().getTime(),
     }
 
     setTodos([newTodo, ...todos])
     setText("")
   }
   return (
+    <>
     <form onSubmit={(e) => handleOnSubmit(e)}>
       <input type="text" value={text}  onChange={(e) => setText(e.target.value)} />
       <input type="submit" value="追加" onSubmit={(e) => handleOnSubmit(e)} />
     </form>
+    <ul>
+        {todos.map((todo) => {
+          return <li key={todo.id}>{todo.value}</li>
+        })}
+    </ul>
+    </>
   );
 }
 
