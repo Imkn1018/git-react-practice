b753cdaf098460abea8df9d485eadbbc573e2a3b
今井健太郎 <imaikentaro@bitA01287.local>
2021-09-14 14:44:35 +0900

    feat: handleOnSubmit関数の追加

--- src/App.tsx
+++ src/App.tsx
@@ -9,10 +9,22 @@ interface Todo {
 function App() {
   const [text, setText] = useState("")
   const [todos, setTodos] = useState<Array<Todo>>([])
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement | HTMLInputElement>) => {
+    e.preventDefault();
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    }
+
+    setTodos([newTodo, ...todos])
+    setText("")
+  }
   return (
-    <form onSubmit={(e) => e.preventDefault()}>
+    <form onSubmit={(e) => handleOnSubmit(e)}>
       <input type="text" value={text}  onChange={(e) => setText(e.target.value)} />
-      <input type="submit" value="追加" onSubmit={(e) => e.preventDefault()} />
+      <input type="submit" value="追加" onSubmit={(e) => handleOnSubmit(e)} />
     </form>
   );
 }
